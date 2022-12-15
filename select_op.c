435eb2049b145f6a8ce617e2d31ad4d13d375eca
cloobTech <belkid98@gmail.com>
2022-12-15 07:08:19 -0800

    monty

---
+++ select_op.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+void (*get_stack_call(char *token))( stack_t **stack, unsigned int line_number)
+{
+	int i = 0;
+	instruction_t op_code[] = {
+		{"push", push_stack},
+		{"pall", print_stack},
+		{"pop", pop_stack},
+		{NULL, NULL}
+	};
+
+	while (op_code[i].opcode)
+	{
+		if (strcmp(token, op_code[i].opcode) == 0)
+			return op_code[i].f;
+		i++;
+	}
+
+	if (op_code[i].opcode == NULL)
+	{
+		printf("Error\n");
+	}
+
+	return (0);
+}
---
+++ stack_calls.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+void push_stack(stack_t **stack, unsigned int line_number)
+{
+	printf("Push is working...\n");	
+}
+
+
+void pop_stack(stack_t **stack, unsigned int line_number)
+{
+	printf("Pop\n");
+}
+
+void print_stack(stack_t **stack, unsigned int line_number)
+{
+	printf("Print Pall\n");	
+}
