739e13b7cd1df9f9328ed44d7f21e7d8c8988c9f
cloobTech <belkid98@gmail.com>
2022-12-17 16:11:08 +0100

    monty

--- monty.h
+++ monty.h
@@ -41,8 +41,9 @@ void push_stack(stack_t **stack, unsigned int line_number);
 void pop_stack(stack_t **stack, unsigned int line_number);
 void print_stack(stack_t **stack, unsigned int line_number);
 stack_t *add_dnodeint(stack_t **head, int n);
-size_t print_dlistint(const stack_t *h);
+void print_dlistint(const stack_t *h);
 int pop_dnodeint(stack_t **head);
 void print_stack_head(stack_t **stack, unsigned int line_number);
 void print_top_node(const stack_t *h);
+size_t len_dlistint(const stack_t *h);
 #endif /*MONTY_H*/
--- stack_calls.c
+++ stack_calls.c
@@ -13,7 +13,7 @@ void push_stack(stack_t **stack, unsigned int line_number)
 
 void pop_stack(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL)
+	if (len_dlistint(*stack) == 0)
 	{
 		dprintf(2, "L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
@@ -24,7 +24,7 @@ void pop_stack(stack_t **stack, unsigned int line_number)
 void print_stack(stack_t **stack, unsigned int line_number)
 {
 	(void)line_number;
-	if (*stack == NULL)
+	if (len_dlistint(*stack) == 0)
 		dprintf(1,"0\n");
 	print_dlistint(*stack);
 }
@@ -34,7 +34,7 @@ void print_stack_head(stack_t **stack, unsigned int line_number)
 {
 	int k = 0;
 
-	if (*stack == NULL)
+	if (len_dlistint(*stack) == 0)
 	{
 		dprintf(2, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
